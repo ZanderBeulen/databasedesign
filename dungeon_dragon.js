98c3dbbf59a429be35afa05c7f721e510763860e
Kolby0359 <45016092+Kolby0359@users.noreply.github.com>
2018-11-14 16:18:12 -0600

    Add files via upload

---
+++ dungeon_dragon.js
@@ -0,0 +1,6 @@
+//dungeon_dragon.js
+// don't forget to validate at jslint.com
+
+/*jslint devel: true, browser: true */
+/*global $*/
+//Figure out code for a drop down system, NOT A MENU, where you can view the database information but not interact with it.
